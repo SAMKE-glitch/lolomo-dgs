049a418db49d16c244c59f1f4eb0dc5ced01c58f
samke-glitch <sammythemwa@gmail.com>
2025-03-10 11:55:39 +0300

    new: initial commit for dgs federation microservice

---
+++ src/main/java/com/netflix/lolomodemo/LolomodemoApplication.java
@@ -0,0 +1,13 @@
+package com.netflix.lolomodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolomodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolomodemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netflix/lolomodemo/LolomodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.lolomodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolomodemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
